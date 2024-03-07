211baa4825d7d88ad63a1ef70ff6e030e6033096
MohammedAnisMultani <147980347+MohammedAnisMultani@users.noreply.github.com>
2024-03-07 09:24:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+const colorSelect = document.getElementById('colorSelect');
+const removeBtn = document.querySelector("input");
+
+removeBtn.addEventListener("click", avojao)
+
+function avojao(e){
+    e.preventDefault();
+    const options = colorSelect.options;
+
+    let selectedIndex = -1;
+    for(let i=0; i<options.length; i++){
+        if(options[i].selected){
+            selectedIndex = i;
+            break;
+        }
+
+    }
+
+    if(selectedIndex==-1){
+       colorSelect.remove(selectedIndex);
+    }
+
+}
+
